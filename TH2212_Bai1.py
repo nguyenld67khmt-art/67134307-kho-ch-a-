90a46ba30660734202a37e07e9a4437e25125675
nguyenld67khmt-art <nguyen.ld.67khmt@ntu.edu.vn>
2025-12-22 14:05:26 +0700

    Create TH2212_Bai1.py
     bai1

---
+++ TH2212_Bai1.py
@@ -0,0 +1,6 @@
+w=float(input("nhap chieu dai hinh chu nhat: "))
+h=float(input("nhap chieu rong hinh chu nhat: "))
+S=w*h
+P=(w+h)*2
+print(f"Chu vi hinh chu nhat la: {P:.2f}")
+print(f"Dien tich hinh chu nhat la: {S:.2f}")
