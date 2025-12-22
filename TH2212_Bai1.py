978ce478be112e897b36e0061a65ded113a8d245
nguyenld67khmt-art <nguyen.ld.67khmt@ntu.edu.vn>
2025-12-22 14:12:54 +0700

    Update TH2212_Bai1.py

--- TH2212_Bai1.py
+++ TH2212_Bai1.py
@@ -1,5 +1,13 @@
 w=float(input("nhap chieu dai hinh chu nhat: "))
 h=float(input("nhap chieu rong hinh chu nhat: "))
+w=float(input("nhap do dai canh 1 hinh chu nhat: "))
+h=float(input("nhap do dai canh 2 hinh chu nhat: "))
+while w <= 0 or w>=100 :
+    print("khong hop le")
+    w=float(input("nhap do dai canh 1 hinh chu nhat: "))
+while h <= 0 or h>=100 :
+    print("khong hop le")
+    h=float(input("nhap do dai canh 2 hinh chu nhat: "))
 S=w*h
 P=(w+h)*2
 print(f"Chu vi hinh chu nhat la: {P:.2f}")
